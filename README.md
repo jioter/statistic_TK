8f2e335b445a46630e85610856b6f9b18f62fb7f
jioter <hreshchuk@gmail.com>
2020-06-10 02:26:35 +0300

    • minor corrections • temporary add timer for functions

---
+++ README.md
@@ -0,0 +1,16 @@
+# Notes and TODO:
+#  table names link to long name +
+
+# Імя листа має бути однаковим
+# consider sheet_name
+
+# Витягує комбінації по перших 6-ти колонках
+
+# Problem with query 8, because all data separated by coma in single cell
+
+# Add cell (0,0) table name "Збалансований розвиток міст України"
+# is it necessary?
+
+1. Change PostgreSQL to SQLite
+2. file set_up_db import xlrd to openpyxl
+# add docstring to all functions (@ - return)
--- standart_query_filtered.py
+++ standart_query_filtered.py
@@ -1,39 +1,42 @@
 import openpyxl
 import psycopg2
+
 database = psycopg2.connect(database="test", user="user1", password="password", host="localhost", port="5432")
 cursor = database.cursor()
 
 
 def query_filter(col_name, filter_col_1, filter_val_1,
-                           filter_col_2, filter_val_2,
-                           filter_col_3, filter_val_3,
-                           filter_col_4, filter_val_4,
-                           filter_col_5, filter_val_5,
-                           filter_col_6, filter_val_6):
-
+                 filter_col_2, filter_val_2,
+                 filter_col_3, filter_val_3,
+                 filter_col_4, filter_val_4,
+                 filter_col_5, filter_val_5,
+                 filter_col_6, filter_val_6):
+    """
+      Filter data by all combinations in first 6 columns.
+      Select poll question and count sum of certain answers to this question.
+      :param col_name:
+      :return: certain question and sum of answers
+      """
     # separate query for column 8
     if col_name == 'col_8':
-        get_col_name_8 = """SELECT col_8 FROM test WHERE id_count=2 GROUP BY col_8"""
+        get_col_name_8 = """SELECT %s FROM test WHERE id_count=2""" % col_name
         cursor.execute(get_col_name_8)
         col_name_8 = [item for t in cursor.fetchall() for item in t]
-
-
 
         query8 = """SELECT DISTINCT(%s) FROM test 
         WHERE %s <> '' AND id_count > 3 AND %s NOT IN ('-') 
         AND %s=%s AND %s=%s AND %s=%s AND %s=%s AND %s=%s AND %s=%s""" \
-                     % (col_name, col_name, col_name,
-                        filter_col_1, "'"+filter_val_1+"'",
-                        filter_col_2, "'"+filter_val_2+"'",
-                        filter_col_3, "'"+filter_val_3+"'",
-                        filter_col_4, "'"+filter_val_4+"'",
-                        filter_col_5, "'"+filter_val_5+"'",
-                        filter_col_6, "'"+filter_val_6+"'")
+                 % (col_name, col_name, col_name,
+                    filter_col_1, "'" + filter_val_1 + "'",
+                    filter_col_2, "'" + filter_val_2 + "'",
+                    filter_col_3, "'" + filter_val_3 + "'",
+                    filter_col_4, "'" + filter_val_4 + "'",
+                    filter_col_5, "'" + filter_val_5 + "'",
+                    filter_col_6, "'" + filter_val_6 + "'")
         cursor.execute(query8)
         records8 = cursor.fetchall()
         records8 = [item for t in records8 for item in t]
         records8 = [el.replace("'", "''") for el in records8]
-
 
         # split elements in sublists
         rez = [el.split(',') for el in records8]
@@ -68,25 +71,24 @@ def query_filter(col_name, filter_col_1, filter_val_1,
                 book.save("data/statistics_calculated.xlsx")
 
     else:
-        get_col_name = """SELECT %s FROM test WHERE id_count=2 GROUP BY %s """ % (col_name, col_name)
+        get_col_name = """SELECT %s FROM test WHERE id_count=2""" % col_name
         cursor.execute(get_col_name)
         col_name_full = [item for t in cursor.fetchall() for item in t]
 
-        get_sub_col_name = """SELECT %s FROM test WHERE id_count=3 GROUP BY %s """ % (col_name, col_name)
+        get_sub_col_name = """SELECT %s FROM test WHERE id_count=3""" % col_name
         cursor.execute(get_sub_col_name)
         sub_col_name_full = [item for t in cursor.fetchall() for item in t]
-
 
         query_data = """SELECT DISTINCT(%s) FROM test 
         WHERE %s <> '' AND id_count > 3 
         AND %s=%s AND %s=%s AND %s=%s AND %s=%s AND %s=%s AND %s=%s""" \
                      % (col_name, col_name,
-                        filter_col_1, "'"+filter_val_1+"'",
-                        filter_col_2, "'"+filter_val_2+"'",
-                        filter_col_3, "'"+filter_val_3+"'",
-                        filter_col_4, "'"+filter_val_4+"'",
-                        filter_col_5, "'"+filter_val_5+"'",
-                        filter_col_6, "'"+filter_val_6+"'")
+                        filter_col_1, "'" + filter_val_1 + "'",
+                        filter_col_2, "'" + filter_val_2 + "'",
+                        filter_col_3, "'" + filter_val_3 + "'",
+                        filter_col_4, "'" + filter_val_4 + "'",
+                        filter_col_5, "'" + filter_val_5 + "'",
+                        filter_col_6, "'" + filter_val_6 + "'")
         cursor.execute(query_data)
         global data2
         data2 = cursor.fetchall()
@@ -96,6 +98,7 @@ def query_filter(col_name, filter_col_1, filter_val_1,
         if not data2:
             pass
         else:
+            #  Check if column name is not empty.
             if not col_name_full == ['']:
                 book = openpyxl.load_workbook("data/statistics_calculated.xlsx")
                 sheet = book.active
@@ -108,13 +111,10 @@ def query_filter(col_name, filter_col_1, filter_val_1,
                 book.save("data/statistics_calculated.xlsx")
 
             for el in data2:
-                cursor.execute("""SELECT %s,Count(*) FROM test WHERE %s='%s' GROUP BY %s """ % (col_name, col_name, el, col_name))
+                cursor.execute(
+                    """SELECT %s,Count(*) FROM test WHERE %s='%s' GROUP BY %s """ % (col_name, col_name, el, col_name))
                 rez = cursor.fetchall()
                 book = openpyxl.load_workbook("data/statistics_calculated.xlsx")
                 sheet = book.active
                 sheet.append(rez[0])
                 book.save("data/statistics_calculated.xlsx")
-
-
-        #  Check if column name is not empty.
-
